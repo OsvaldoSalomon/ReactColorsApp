cec3b735a1b2e37a7e0ab47e9db48bd933609069
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-09-11 11:14:16 -0400

    Added the ColorHelper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 import Palette from "./components/Palette";
 import seedColors from "./components/seedColors";
+import {generatePalette} from "./components/colorHelpers";
 
-function App() {
-    return (
-        <div>
-            <Palette {...seedColors[2]}/>
-        </div>
-    );
+class App extends Component {
+    render() {
+        console.log(generatePalette(seedColors[2]));
+        return (
+            <div>
+                <Palette {...seedColors[2]}/>
+            </div>
+        );
+    }
 }
 
 export default App;
