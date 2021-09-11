90262dca1520bfb8b8bcb39ccf11291cd6e0ce09
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-09-11 10:24:00 -0400

    Added a lot the colors, and the overlay, everything is working so far.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import Palette from "./components/Palette";
 import seedColors from "./components/seedColors";
 
 function App() {
     return (
         <div>
-            <Palette {...seedColors[4]}/>
+            <Palette {...seedColors[2]}/>
         </div>
     );
 }
