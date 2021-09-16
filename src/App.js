8e7f6cdee8496eb6038fcde451ced82a12565b79
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-09-16 08:18:51 -0400

    Added palette's footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
         console.log(generatePalette(seedColors[2]));
         return (
             <div>
-                <Palette palette={generatePalette(seedColors[4])}/>
+                <Palette palette={generatePalette(seedColors[2])}/>
             </div>
         );
     }
