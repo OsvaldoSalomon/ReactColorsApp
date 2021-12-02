fb5f0e8f6b59d2426bbaed69f3c14dbf60d23f61
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-12-02 05:47:38 -0500

    Fixed navbar and add color to ColorBox

--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import {CopyToClipboard} from "react-copy-to-clipboard/lib/Component";
 import {withStyles} from "@material-ui/styles";
+import classNames from "classnames";
 import styles from './styles/ColorBoxStyles';
-
 
 class ColorBox extends Component {
     constructor(props) {
@@ -25,8 +25,9 @@ class ColorBox extends Component {
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{background}} className={classes.ColorBox}>
                     <div style={{background}}
-                         className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}/>
-                    <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
+                         className={classNames(classes.copyOverlay, {[classes.showOverlay]: copied})}/>
+                    <div
+                        className={classNames(classes.copyMessage, {[classes.showMessage]: copied})}>
                         <h1>copied!</h1>
                         <p className={classes.copyText}>{this.props.background}</p>
                     </div>
